378aed8d1db52415733d84655f9d1c77a6cf4f23
Michael <gordonbay@gmail.com>
2016-05-14 20:44:44 -0300

    14/05/2016 update

--- data/actions/scripts/other/potions.lua
+++ data/actions/scripts/other/potions.lua
@@ -5,6 +5,8 @@ local config = {
 	[7589] = {mana = {min = 115, max = 185}, vocations = {1, 2, 3}, text = 'sorcerers, druids and paladins', level = 50, emptyId = 7634},
 	-- great mana potion
 	[7590] = {mana = {min = 150, max = 250}, vocations = {1, 2}, text = 'sorcerers and druids', level = 80, emptyId = 7635},
+	-- Ultimate Mana Potion
+	[26029] = {mana = {min = 400, max = 600}, vocations = {1, 2}, text = 'sorcerers and druids', level = 130, emptyId = 7635},
 	-- great health potion
 	[7591] = {health = {min = 425, max = 575}, vocations = {4}, text = 'knights', level = 80, emptyId = 7635},
 	-- health potion
@@ -13,8 +15,12 @@ local config = {
 	[7620] = {mana = {min = 75, max = 125}, emptyId = 7636},
 	-- great spirit potion
 	[8472] = {health = {min = 250, max = 350}, mana = {min = 100, max = 200}, vocations = {3}, text = 'paladins', level = 80, emptyId = 7635},
+	-- Ultimate Spirit Potion
+	[26030] = {health = {min = 400, max = 550}, mana = {min = 150, max = 250}, vocations = {3}, text = 'paladins', level = 130, emptyId = 7635},
 	-- ultimate health potion
 	[8473] = {health = {min = 650, max = 850}, vocations = {4}, text = 'knights', level = 130, emptyId = 7635},
+	-- supreme health potion
+	[26031] = {health = {min = 800, max = 1000}, vocations = {4}, text = 'knights', level = 200, emptyId = 7635},
 	-- antidote potion
 	[8474] = {antidote = true, emptyId = 7636},
 	-- small health potion
@@ -41,6 +47,9 @@ function onUse(player, item, fromPosition, target, toPosition, isHotkey)
 	if target.itemid ~= 1 or target.type ~= THING_TYPE_PLAYER then
 		return false
 	end
+
+
+
 
 	if player:getCondition(CONDITION_EXHAUST_HEAL) then
 		player:sendTextMessage(MESSAGE_STATUS_SMALL, Game.getReturnMessage(RETURNVALUE_YOUAREEXHAUSTED))
