d61b68c83d11f2237c6adf77932fbc3d241cb312
Michael <gordonbay@gmail.com>
2016-05-20 16:28:12 -0300

    20/05/2016 update B

--- data/movements/scripts/quests/the new frontier/zaosteppe.lua
+++ data/movements/scripts/quests/the new frontier/zaosteppe.lua
@@ -4,7 +4,7 @@ function onStepIn(creature, item, position, fromPosition)
 		return true
 	end
 
-	if player:getStorageValue(Storage.TheNewFrontier.TomeofKnowledge) >= 6 then		
+	if player:getStorageValue(Storage.TheNewFrontier.TomeofKnowledge) >= 7 then		
 		player:teleportTo(Position(33198, 31346, 6))		
 	else
 		player:teleportTo(Position(33145, 31248, 6))
